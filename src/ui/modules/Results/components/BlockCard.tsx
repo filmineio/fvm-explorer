58a26ea8db4ead7f0c61e255d079d8a67dd2bc2b
NemanjaAltLabs <nemanja.radovanovic@altlabs.dev>
2023-02-23 15:52:10 +0100

    - Restyle block card

--- src/ui/modules/Results/components/BlockCard.tsx
+++ src/ui/modules/Results/components/BlockCard.tsx
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import { CopyWrapper } from "@/ui/components/CopyWrapper/CopyWrapper";
 
 import { Block } from "@/types/data/Block";
+import BlockIcon from "@/ui/components/Common/Icons/BlockIcon";
 
 
 type BlockCardProps = {
@@ -19,39 +20,39 @@ export const BlockCard = ({ data, network }: BlockCardProps) => {
   );
 
   return (
-    <div className="w-full sm:min-w-full max-w-xs md:w-1/2 lg:w-1/3xs:w-full my-2 sm:pr-5 px-0 cursor-pointer">
+    <div className="w-full sm:min-w-full max-w-xs md:w-1/2 lg:w-1/3xs:w-full sm:pr-5 px-0 cursor-pointer">
       <Link href={`/explore/${Entity.Block}/${data.cid}?network=${network}`}>
-        <div className="relative flex flex-col min-w-0 break-words bg-slate border-2 border-slate hover:border-label rounded-base  shadow-lg">
+        <div className="relative flex flex-col break-words bg-body_opacity-50 border-2 border-transparent hover:border-label rounded-base shadow-lg">
           <div className="flex-auto p-5">
             <div className="flex flex-wrap items-center">
               <div className="relative pr-4 w-4/12">
-                <div className="bg-bglight rounded-base py-3 px-3 flex justify-center items-center">
-                  <img src="/images/block-icon.svg" alt={""} />
+                <div className="flex bg-label_opacity-30 rounded-msm justify-center items-center w-20 h-20">
+                  <BlockIcon/>
                 </div>
               </div>
-              <div className="relative w-8/12 flex flex-col gap-2">
+              <div className="relative w-8/12">
                 <CopyWrapper data={data.cid}>
-                  <h4 className="text-white leading-6 text-base font-bold font-roboto truncate	">
+                  <h4 className="font-roboto text-white text-lg font-bold leading-compact truncate">
                     {data.cid}
                   </h4>
                 </CopyWrapper>
 
-                <p className="text-sm text-white leading-4 font-space">
+                <p className="font-roboto text-white text-sm font-normal leading-normal tracking-wider mt-1">
                   Height: {data.height}
                 </p>
               </div>
             </div>
 
-            <div className="flex flex-col mt-3 gap-2">
+            <div className="flex flex-col mt-5 gap-2">
               <div className="w-full">
-                <div className="text-gray-text font-normal	text-sm	 tracking-wider">
+                <div className="text-label font-normal text-sm tracking-wider ">
                   <span>BLOCK SIGNATURE: </span>
-                  <button className="bg-bglight rounded-base px-2 py-1 font-medium font-space	text-sm	leading-5 text-white truncate max-w-xs">
+                  <button className="bg-label_opacity-30 rounded-msm px-2 py-1 font-roboto font-normal text-sm leading-5 text-blue-400 truncate max-w-xs">
                     Type {block.BlockSig?.Type}
                   </button>
                 </div>
               </div>
-              <div className="text-white text-sm truncate">
+              <div className="font-roboto text-white text-sm font-normal leading-compact truncate">
                 {block.BlockSig?.Data}
               </div>
             </div>
