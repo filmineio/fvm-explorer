6a7d8fc05a1ab2df109551a1d9b279a60af1714f
NemanjaAltLabs <nemanja.radovanovic@altlabs.dev>
2023-02-23 12:23:25 +0100

    - Refactor icons

--- src/ui/components/Common/Icons/ArrowChevronPrevious.tsx
+++ src/ui/components/Common/Icons/ArrowChevronPrevious.tsx
@@ -2,20 +2,8 @@ import React from "react";
 
 function ArrowChevronPrevious() {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="7"
-            height="12"
-            fill="none"
-            viewBox="0 0 7 12"
-        >
-            <path
-                stroke="#D8DBE9"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5.642 10.284L1 5.642 5.642 1"
-            ></path>
+        <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" fill="none" viewBox="0 0 7 12">
+            <path stroke="#D8DBE9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.642 10.284L1 5.642 5.642 1"></path>
         </svg>
     );
 }
