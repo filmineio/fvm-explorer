4f523d27ae310721f0de77b6c78be57c59c7dc2d
rasha08 <rasha08@gmail.com>
2023-02-25 09:06:48 +0100

    GQL Editor

--- pages/me/queries/index.tsx
+++ pages/me/queries/index.tsx
@@ -1,19 +1,35 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 
 import {
   MyDataKind,
   MyDataWrapper,
 } from "@/ui/components/MyDataWrapper/MyDataWrapper";
+import { Spinner } from "@/ui/components/Spinner/Spinner";
+
+import { GraphQLEditor } from "@/ui/modules/GraphQl/GraphQLEditor";
+
+import { cb } from "@/utils/cb";
+
 
 export default function Queries(): ReactElement {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(cb(setLoaded, true), []);
+
+  if (!loaded)
+    return (
+      <MyDataWrapper kind={MyDataKind.Queries}>
+        <div className=" all px-0 max-w-[90%] pl-[2%] justify-self-center mx-auto pt-20 pb-10 min-h-calc text-gray-text">
+          <Spinner />
+        </div>
+      </MyDataWrapper>
+    );
+
   return (
     <MyDataWrapper kind={MyDataKind.Queries}>
-      <div
-        className={
-          "w-full min-h-calc flex  justify-center items-center gap-24 text-white"
-        }
-      >
-        <h1>Queries</h1>
+      <div className=" all px-0 max-w-[90%] pl-[2%] justify-self-center mx-auto pt-20 pb-10 min-h-calc relative">
+        <div className={"text-2xl text-white mb-4"}>My Queries</div>
+        <GraphQLEditor />
       </div>
     </MyDataWrapper>
   );
