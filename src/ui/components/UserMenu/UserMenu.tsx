18cc2c3bbc5a2df0c3170daa2c5466d61d1773c1
NemanjaAltLabs <nemanja.radovanovic@altlabs.dev>
2023-02-26 00:58:36 +0100

    - Remove more unnecessary imports

--- src/ui/components/UserMenu/UserMenu.tsx
+++ src/ui/components/UserMenu/UserMenu.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 
 import { User } from "@/ui/state/types/AppState";
 
