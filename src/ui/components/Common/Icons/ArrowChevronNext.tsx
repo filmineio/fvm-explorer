904f58c0605d7f9a7aace37ff9740d6d030c2ce6
NemanjaAltLabs <nemanja.radovanovic@altlabs.dev>
2023-02-25 10:54:25 +0100

    - Refactor code

--- src/ui/components/Common/Icons/ArrowChevronNext.tsx
+++ src/ui/components/Common/Icons/ArrowChevronNext.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { FC } from "react";
 
-function ArrowChevronNext() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" fill="none" viewBox="0 0 7 12">
-            <path stroke="#D8DBE9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 10.284l4.642-4.642L1 1"></path>
-        </svg>
-    );
+const ArrowChevronNext: FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" fill="none" viewBox="0 0 7 12">
+      <path stroke="#D8DBE9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 10.284l4.642-4.642L1 1"></path>
+    </svg>
+  );
 }
 
 export default ArrowChevronNext;
--- src/ui/components/Common/Icons/AuthIllustrationBottom.tsx
+++ src/ui/components/Common/Icons/AuthIllustrationBottom.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { FC } from "react";
 
-function AuthIllustrationBottom() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="529" height="499" fill="none" viewBox="0 0 529 499">
-            <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(-.96593 .25882 -.7071 -.7071 377.633 462.431)"></rect>
-            <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(.96593 -.25882 -.7071 -.7071 117.125 403.423)"></rect>
-            <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(-.96593 .25882 .7071 .7071 243.921 199.291)"></rect>
+const AuthIllustrationBottom: FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="529" height="499" fill="none" viewBox="0 0 529 499">
+      <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(-.96593 .25882 -.7071 -.7071 377.633 462.431)"></rect>
+      <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(.96593 -.25882 -.7071 -.7071 117.125 403.423)"></rect>
+      <rect width="382.91" height="260.409" fill="#D8DBE9" rx="10" transform="matrix(-.96593 .25882 .7071 .7071 243.921 199.291)"></rect>
 
-            <rect width="382.91" height="260.409" style={{ mixBlendMode: 'multiply' }} fill="#F0A108" rx="10" transform="matrix(-.96593 .25882 -.7071 -.7071 377.633 462.431)"></rect>
-            <rect width="382.91" height="260.409" style={{ mixBlendMode: 'multiply' }} fill="#0576F0" rx="10" transform="matrix(.96593 -.25882 -.7071 -.7071 117.125 403.423)"></rect>
-            <rect width="382.91" height="260.409" style={{ mixBlendMode: 'multiply' }} fill="#D857C9" rx="10" transform="matrix(-.96593 .25882 .7071 .7071 243.921 199.291)"></rect>
-        </svg>
-    );
+      <rect width="382.91" height="260.409" fill="#F0A108" style={{ mixBlendMode: 'multiply' }} rx="10" transform="matrix(-.96593 .25882 -.7071 -.7071 377.633 462.431)"></rect>
+      <rect width="382.91" height="260.409" fill="#0576F0" style={{ mixBlendMode: 'multiply' }} rx="10" transform="matrix(.96593 -.25882 -.7071 -.7071 117.125 403.423)"></rect>
+      <rect width="382.91" height="260.409" fill="#D857C9" style={{ mixBlendMode: 'multiply' }} rx="10" transform="matrix(-.96593 .25882 .7071 .7071 243.921 199.291)"></rect>
+    </svg>
+  );
 
 }
 export default AuthIllustrationBottom;
