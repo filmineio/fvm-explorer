90a9f5ea15b7f6cffca83001f99bdd07a1d31765
Niko Beretic <nikoberetic@gmail.com>
2023-03-01 17:26:19 +0100

    - Advanced filter selection

--- src/ui/modules/Filters/Filters.tsx
+++ src/ui/modules/Filters/Filters.tsx
@@ -10,15 +10,20 @@ import { useLocationQuery } from "@/ui/hooks/useLocationQuery";
 import { useStore } from "@/ui/state/Store";
 import { setFiltersValueTransformer } from "@/ui/state/transformers/filters/setFiltersValueTransformer";
 import { AdvancedFiltersState } from "@/ui/state/types/AppState";
+import Button from "@/ui/components/Button";
+import Link from "next/link";
+import router from "next/router";
 
 
 export const Filters = ({ search }: { search: () => void }) => {
   const query = useLocationQuery<NextParsedUrlQuery>();
 
-  const [advancedSearchActive, toggleAdvancedSearch] = useState(false);
+  const [searchSelectionActive, setSearchSelectionActive] = useState(false);
+  const [advancedSearchActive, setAdvancedSearchActive] = useState(false);
+  const [init, setInit] = useState(true);
   const {
     mod,
-    state: { filters: state },
+    state: { user, filters: state },
   } = useStore();
 
   const change = useCallback(
@@ -28,21 +33,53 @@ export const Filters = ({ search }: { search: () => void }) => {
     [mod]
   );
 
+  const existingAdvancedFilters = !!query.advancedFilter;
+
   useEffect(() => {
-    toggleAdvancedSearch(!!query.advancedFilter);
+    setSearchSelectionActive(existingAdvancedFilters);
+    setAdvancedSearchActive(existingAdvancedFilters);
+    setInit(false);
   }, []);
 
+  useEffect(() => {
+    if (!init && !searchSelectionActive) {
+      setAdvancedSearchActive(false);
+
+      if (existingAdvancedFilters) {
+        router.push('/');
+      }
+    }
+  }, [searchSelectionActive]);
+
+  const toggleAdvancedSearchActive = () => {
+    setAdvancedSearchActive(prevState => !prevState);
+  };
+
   return (
-    <div className="pt-10 space-y-5 ">
+    <div className="pt-5 mt-5">
       <FiltersHeader
         state={state}
-        checked={advancedSearchActive}
-        toggle={toggleAdvancedSearch}
+        checked={searchSelectionActive}
+        toggle={setSearchSelectionActive}
         search={search}
       />
-      {advancedSearchActive ? (
-        <AdvancedFilters handleChange={change} state={state} onClick={search} />
-      ) : (
+      {searchSelectionActive ? (
+        advancedSearchActive ? (
+          <AdvancedFilters handleChange={change} state={state} onClick={search} />
+        ) : (
+          <div className="flex gap-5 items-center justify-center p-11 border-2 border-label rounded-6 mt-6 mb-8">
+            <Button className="btn border-2 bg-blue-500 border-blue-500 text-white hover:bg-blue-400 hover:border-blue-400 active:shadow-[0px_0px_0px_3px_rgba(89,169,255,0.3)] transition-all" onClick={toggleAdvancedSearchActive}>QUERY BUILDER</Button>
+            {user ? (
+              <Link href="/me/queries">
+                <Button className="btn border-2 bg-slate border-blue-500 text-blue-500 hover:text-blue-400 hover:border-blue-400 active:shadow-[0px_0px_0px_3px_rgba(89,169,255,0.3)] transition-all">GRAPHQL SEARCH</Button>
+              </Link>
+              ) : (
+              <Link href="/auth?redirect=queries">
+                <Button className="btn border-2 bg-slate border-blue-500 text-blue-500 hover:text-blue-400 hover:border-blue-400 active:shadow-[0px_0px_0px_3px_rgba(89,169,255,0.3)] transition-all">GRAPHQL SEARCH</Button>
+              </Link>
+            )}
+          </div>
+      )) : (
         <SearchFilters handleChange={change} state={state} onClick={search} />
       )}
     </div>
