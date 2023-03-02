416ba9f4c8fc3eb4f5757315203786182f3f3f1b
Niko Beretic <nikoberetic@gmail.com>
2023-03-02 08:22:41 +0100

    - Meta info

--- src/ui/components/Page/Page.tsx
+++ src/ui/components/Page/Page.tsx
@@ -2,7 +2,7 @@ import { Footer } from "./Footer/Footer";
 import { HeaderComponents } from "./Header";
 import Head from "next/head";
 import React, { FC } from "react";
-import { META_PAGE_TITLE } from "@/constants/global";
+import { META_IMAGE_URL, META_PAGE_DESCRIPTION, META_PAGE_TITLE, META_PAGE_URL } from "@/constants/global";
 
 export type PageProps = {
   title?: string;
@@ -17,9 +17,9 @@ export const Page: FC<PageProps> = ({
   showHeader,
   showFooter,
 }) => {
-  const description = 'Filecoin network explorer';
-  const link = 'https://explorer.filmine.io';
-  const metaImageLink = '';
+  const description = META_PAGE_DESCRIPTION;
+  const url = META_PAGE_URL;
+  const metaImageLink = META_IMAGE_URL;
 
   return (
     <div
@@ -35,16 +35,16 @@ export const Page: FC<PageProps> = ({
         <meta name="theme-color" content="#000000" />
         <meta name="title" content={title} key="metaTitle" />
         <meta name="description" content={description} key="metaDescription" />
-        <link rel="canonical" href={link} />
+        <link rel="canonical" href={url} />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={link} key="metaOGUrl" />
+        <meta property="og:url" content={url} key="metaOGUrl" />
         <meta property="og:title" content={title} key="metaOGTitle" />
         <meta property="og:description" content={description} key="metaOGDescription" />
         <meta property="og:image" content={metaImageLink} key="metaOGImage" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={link} key="metaTwitterUrl" />
+        <meta property="twitter:url" content={url} key="metaTwitterUrl" />
         <meta property="twitter:title" content={title} key="metaTwitterTitle" />
         <meta property="twitter:image" content={metaImageLink} key="metaTwitterImage" />
         <meta property="twitter:description" content={description} key="metaTwitterDescription" />
