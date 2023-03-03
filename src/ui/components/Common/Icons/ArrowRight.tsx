7e9e4c5b43307209eb9e890467d5424726e6a521
NemanjaAltLabs <nemanja.radovanovic@altlabs.dev>
2023-03-03 18:03:45 +0100

    - Fix border radius

---
+++ src/ui/components/Common/Icons/ArrowRight.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const ArrowRight: FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="12" fill="none" viewBox="0 0 21 12">
+    <path stroke="#D8DBE9" strokeLinecap="square" strokeWidth="2" d="M19.389 6l-4.334 4.5M19.39 6l-4.334-4.5M19.39 6H1.333"></path>
+  </svg>
+);
+
+export default ArrowRight;
