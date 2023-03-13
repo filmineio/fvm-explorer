72dfa67b7312a8d2d6b45e3bbce24bc7379f4b76
rasha08 <rasha08@gmail.com>
2023-03-13 07:43:48 +0100

    stats endpoints

---
+++ src/api/stats/internal/getLatestTipSets.ts
@@ -0,0 +1,33 @@
+import { Entity } from "@/enums/Entity";
+import { Network } from "@/enums/Network";
+import { groupBy, pickAll } from "ramda";
+
+import { Block } from "@/types/data/Block";
+
+import { ApiCtx } from "@/api/ctx/apiCtx";
+
+
+type DbResponse<T> = T & Record<"total", number>;
+export const getLatestTipSets = async (nwk: Network, ctx: ApiCtx) => {
+  const data = await ctx.database.ch.data.chain[nwk].query<Block>({
+    pagination: { limit: 50, offset: 0 },
+    order: ["height", "DESC"],
+    fieldName: Entity.Block,
+    selection: ["height", "cid", "miner"],
+    query: [
+      {
+        height: { isNull: false },
+      },
+    ],
+  });
+
+  const v = groupBy<DbResponse<Block>>((a) => a.height.toString(), data);
+
+  return Object.keys(v)
+    .sort((a, b) => +b - +a)
+    .slice(0, 5)
+    .map((z) => ({
+      height: z,
+      blocks: v[z].map(pickAll(["cid", "miner"])),
+    }));
+};
