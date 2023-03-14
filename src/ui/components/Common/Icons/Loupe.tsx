a3863139f3775e8340e9febde3a31472f700f13e
Niko Beretic <nikoberetic@gmail.com>
2023-03-14 16:27:40 +0100

    - Stats progress

---
+++ src/ui/components/Common/Icons/Loupe.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from "react";
+
+const Loupe: FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="none"
+      viewBox="0 0 18 18"
+    >
+      <path
+        stroke="#596184"
+        strokeLinecap="round"
+        strokeWidth="2"
+        d="M17 17l-3.778-3.784L17 17zm-1.684-8.842A7.158 7.158 0 111 8.158a7.158 7.158 0 0114.316 0v0z"
+      />
+    </svg>
+  );
+}
+
+export default Loupe;
