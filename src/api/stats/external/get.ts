c028aa3062abf6edbda700ebb6ce6355262c346e
Srdjan S <stankovic.srdjo@gmail.com>
2023-03-15 10:43:53 +0100

    Add support for filfox mainnet

--- src/api/stats/external/get.ts
+++ src/api/stats/external/get.ts
@@ -1,7 +1,16 @@
 import { Network } from "@/enums/Network";
 import axios from "axios";
 
-const BASE_EXT_URL = (nwk: Network) => `https://${nwk}.filfox.info/api/v1/`;
+const BASE_EXT_URL = (nwk: Network) => {
+  switch (nwk) {
+    case Network.HyperSpace:
+      return `https://${nwk}.filfox.info/api/v1/`;
+    case Network.Mainnet:
+      return `https://filfox.info/api/v1/`;
+    default:
+      return `https://${nwk}.filfox.info/api/v1/`;
+  }
+};
 export const get = async <T>(nwk: Network, path: string, def: T) => {
   try {
     const d = await axios.get(`${BASE_EXT_URL(nwk)}${path}`);
